bf87cf9047b5c20e7abbb43c1b63c449e2cf15d4
Adeleadbe <adbe21@gmail.com>
2023-08-03 10:20:24 +0200

    creation of the show page

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Article;
 use App\Form\ArticleType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,13 +13,17 @@ use Symfony\Component\Routing\Annotation\Route;
 class ArticleController extends AbstractController
 {
     #[Route('/article', name: 'article')]
-    public function index()
+    public function index(EntityManagerInterface $entityManager)
     {
-        return $this->render('articles/index.html.twig');
+        $articles = $entityManager->getRepository(Article::class)->findAll();
+        
+        return $this->render('articles/index.html.twig', [
+            'articles' => $articles
+        ]);
     }
 
     #[Route('/article/new', name: 'article_new')]
-    public function new(Request $request)
+    public function new(Request $request, EntityManagerInterface $manager)
     {
         //Instanciation d'un nouvel objet Article
         $article = new Article();
@@ -27,14 +32,29 @@ class ArticleController extends AbstractController
         //Traitement du formulaire soumis
         $form->handleRequest($request);
 
+        if($form->isSubmitted() && $form->isValid()){
+            $manager->persist($article);
+            $manager->flush();
+
+            return $this->redirectToRoute('app_article');
+        }
+
         return $this->render('articles/new.html.twig', [
             'form' => $form->createView(),
-        ]);
+        ]); 
     }
 
-    //#[Route('/article/{id}', name: 'article')]
-    //public function show()
-    //{
-        //return $this->render('articles/show.html.twig');
-    //}
+    #[Route('/article/{id}', name: 'article_show')]
+    public function show($id, EntityManagerInterface $entityManager)
+    {
+        $article = $entityManager->getRepository(Article::class)->findOneBy(['id' => $id]);
+
+        if(is_null($article)){
+            return $this->redirectToRoute('app_article');
+        }
+
+        return $this->render('articles/show.html.twig', [
+            'article' => $article
+        ]);
+    }
 }
