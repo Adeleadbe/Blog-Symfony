0d1881e63401d30d7d688d15524a3b55057de6a0
Adeleadbe <adbe21@gmail.com>
2023-08-09 11:26:59 +0200

    modify form register

--- src/Controller/Security/RegisterController.php
+++ src/Controller/Security/RegisterController.php
@@ -2,16 +2,25 @@
 
 namespace App\Controller\Security;
 
+use App\Entity\User;
+use App\Form\UserType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route(name: 'app_security_')]
 class RegisterController extends AbstractController
 {
     #[Route('/register', name: 'register')]
-    public function register() 
+    public function register(Request $request) 
     {
-        return $this->render('security/register.html.twig');
+        $user = new User;
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        return $this->render('security/register.html.twig', [
+            'form'=>$form->createView()
+        ]);
     }
 
 }
