596db14fab14d3482b63a1241c721efeba7f3f9b
Adeleadbe <adbe21@gmail.com>
2023-08-09 10:48:00 +0200

    add login and modify security.yaml

---
+++ src/Controller/Security/RegisterController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller\Security;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route(name: 'app_security_')]
+class RegisterController extends AbstractController
+{
+    #[Route('/register', name: 'register')]
+    public function register() 
+    {
+        return $this->render('security/register.html.twig');
+    }
+
+}
---
+++ src/Controller/Security/SecurityController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller\Security;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+#[Route(name: 'app_security_')]
+class SecurityController extends AbstractController
+{
+    #[Route('/login', name: 'login')]
+    public function login(Request $request, AuthenticationUtils $authenticationUtils) 
+    {
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', [
+            'error' => $error,
+            'lastUsername' => $lastUsername
+        ]);
+    }
+
+    #[Route('/logout', name: 'logout')]
+    public function logout() {
+        
+    }
+
+}
